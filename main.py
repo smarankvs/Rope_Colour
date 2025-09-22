565bb6fc4b5af9ccf54d55249f7fea92d0382e9f
smarankvs <smarankvs@gmail.com>
2025-09-22 21:01:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,24 @@ from PIL import Image
 import numpy as np
 
 def classify_rope_color(image_path):
-    # Load image
     img = Image.open(image_path).convert('RGB')
     img_resized = img.resize((200, 200))  
 
-    # Extract pixels
     pixels = np.array(img_resized).reshape(-1, 3)
 
-    # Apply KMeans
     kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
     kmeans.fit(pixels)
 
-    # Find dominant cluster
     counts = Counter(kmeans.labels_)
     dominant_cluster = counts.most_common(1)[0][0]
     dominant_color = kmeans.cluster_centers_[dominant_cluster].astype(int)
 
-    # Define reference colors
     color_refs = {'green': [120,170,120], 'yellow': [129,127,118], 'white': [135,136,135]}
     def color_distance(c1, c2):
         return np.linalg.norm(np.array(c1) - np.array(c2))
 
-    # Classify overall color
     overall_color = min(color_refs, key=lambda c: color_distance(dominant_color, color_refs[c]))
     print("Dominant color (RGB):", dominant_color)
     print("Predicted overall rope color:", overall_color)
 
-# Use *forward slashes* or double backslashes in Windows path
 classify_rope_color('D:\\Smaran_Required\\ASPL\\rope_color\\Rope_folders\\20250903150024_IMG_9363.jpg')
